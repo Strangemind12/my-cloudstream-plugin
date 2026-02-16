13458203c67f45c86f3b3d35ea9b63de0b2c7fd3
hsp1020 <125277818+hsp1020@users.noreply.github.com>
2026-02-16 22:01:47 +0900

    Update KotbcPlugin.kt

--- KotbcProvider/src/main/kotlin/com/kotbc/KotbcPlugin.kt
+++ KotbcProvider/src/main/kotlin/com/kotbc/KotbcPlugin.kt
@@ -1,12 +1,16 @@
 package com.kotbc
 
-import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
 
 @CloudstreamPlugin
-class KotbcPlugin: BasePlugin() {
-    override fun load() {
+class KotbcPlugin : Plugin() {
+    override fun load(context: Context) {
+        // [수정] Main Provider는 registerMainAPI 사용
         registerMainAPI(Kotbc())
-        registerMainAPI(KotbcExtractor())
+        
+        // [수정] Extractor는 registerExtractorAPI 사용
+        registerExtractorAPI(KotbcExtractor())
     }
 }
