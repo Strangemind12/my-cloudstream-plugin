450cbdd6a0592d3e948c37e88f321feb9e2f6b1c
hsp1020 <125277818+hsp1020@users.noreply.github.com>
2026-02-17 13:47:46 +0900

    Create build.gradle.kts

---
+++ AnilifeProvider/build.gradle.kts
@@ -0,0 +1,14 @@
+version = 1
+
+cloudstream {
+    description = "애니메이션 한글 자막 제공"
+    authors = listOf("hsp1020") // 본인 계정명으로 변경
+    language = "ko"
+    
+    status = 1 // 0: Down, 1: Ok, 2: Slow, 3: Beta
+    tvTypes = listOf(
+        "Anime"
+    )
+    
+    iconUrl = "https://anilife.live/assets/img/animestream.png"
+}
