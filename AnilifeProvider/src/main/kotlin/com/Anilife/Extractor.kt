f69aa371d8693a62ec03c09635c7ad729ede30c0
hsp1020 <125277818+hsp1020@users.noreply.github.com>
2026-02-17 21:53:16 +0900

    Update Extractor.kt

--- AnilifeProvider/src/main/kotlin/com/Anilife/Extractor.kt
+++ AnilifeProvider/src/main/kotlin/com/Anilife/Extractor.kt
@@ -4,7 +4,7 @@ import com.lagradost.cloudstream3.SubtitleFile
 import com.lagradost.cloudstream3.app
 import com.lagradost.cloudstream3.utils.ExtractorApi
 import com.lagradost.cloudstream3.utils.ExtractorLink
-import com.lagradost.cloudstream3.utils.ExtractorLinkType
+import com.lagradost.cloudstream3.utils.*
 import kotlinx.coroutines.runBlocking
 import java.io.BufferedReader
 import java.io.InputStreamReader
