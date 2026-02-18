d3c149787d4bccd489eec3e44aaa59867452ab1b
hsp1020 <125277818+hsp1020@users.noreply.github.com>
2026-02-19 02:34:55 +0900

    Update DaddyLivePlugin.kt

--- DaddyLiveProvider/src/main/kotlin/com/DaddyLive/DaddyLivePlugin.kt
+++ DaddyLiveProvider/src/main/kotlin/com/DaddyLive/DaddyLivePlugin.kt
@@ -1,4 +1,4 @@
-package it.dogior.hadEnough
+package com.DaddyLive
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
@@ -7,8 +7,9 @@ import android.content.Context
 @CloudstreamPlugin
 class DaddyLivePlugin: Plugin() {
     override fun load(context: Context) {
-        // All providers should be added in this manner
+        // MainAPI 등록
         registerMainAPI(DaddyLiveScheduleProvider())
+        // ExtractorAPI 등록 (이제 Extractor.kt는 WebView를 사용하므로 별도 등록 필요)
         registerExtractorAPI(DaddyLiveExtractor())
     }
 }
