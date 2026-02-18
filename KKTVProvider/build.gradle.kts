e4fa5439117f87358c7d6924511db7868464f664
hsp1020 <125277818+hsp1020@users.noreply.github.com>
2026-02-19 00:00:31 +0900

    Create build.gradle.kts

---
+++ KKTVProvider/build.gradle.kts
@@ -0,0 +1,14 @@
+version = 1
+
+cloudstream {
+    description = "스포츠 생중계 제공"
+    authors = listOf("hsp1020") // 본인 계정명으로 변경
+    language = "ko"
+    
+    status = 1 // 0: Down, 1: Ok, 2: Slow, 3: Beta
+    tvTypes = listOf(
+        "Live"
+    )
+    
+    iconUrl = "https://holyindia.org/assets/Pc/img/hd_logo.png"
+}
