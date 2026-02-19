2f155b37d4ebca9c3bb3573a0b8b75b851d6a4b6
hsp1020 <125277818+hsp1020@users.noreply.github.com>
2026-02-20 07:03:27 +0900

    Update build.gradle.kts

--- StremioX/build.gradle.kts
+++ StremioX/build.gradle.kts
@@ -1,22 +1,19 @@
-// v2.0
-import java.util.Properties
+// v1.10
+import java.util.Properties // org.jetbrains.kotlin... 대신 표준 java.util 사용
 
 // use an integer for version numbers
 version = 13
 
 android {
-    namespace = "com.phisher98" // Gradle 8.0+ 필수 추가 (이 코드가 없으면 BuildConfig가 생성되지 않음)
-
     buildFeatures {
         buildConfig = true
         viewBinding = true
     }
     defaultConfig {
         val propFile = project.rootProject.file("local.properties")
-        
         val tmdbApiKey = if (propFile.exists()) {
             val properties = Properties()
-            properties.load(propFile.inputStream())
+            propFile.inputStream().use { properties.load(it) }
             properties.getProperty("TMDB_API") ?: ""
         } else {
             System.getenv("TMDB_API") ?: ""
@@ -33,21 +30,9 @@ dependencies {
 
 cloudstream {
     language = "en"
-   
- // All of these properties are optional, you can safely remove them
-
-     description = "[!] Requires Setup \n- StremioX allows you to use stream addons \n- StremioC allows you to use catalog addons"
-     authors = listOf("Hexated", "phisher98")
-
-    /**
-     * Status int as the following:
-     * 0: Down
-     * 1: Ok
-     * 2: Slow
-     * 3: Beta only
-     * */
+    description = "[!] Requires Setup \n- StremioX allows you to use stream addons \n- StremioC allows you to use catalog addons"
+    authors = listOf("Hexated,phisher98")
     status = 1 
-// will be 3 if unspecified
     tvTypes = listOf(
         "TvSeries",
         "Movie",
