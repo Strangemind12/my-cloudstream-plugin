1d58e614fd372caf876afa898496b05d401c44bb
hsp1020 <125277818+hsp1020@users.noreply.github.com>
2026-02-20 08:06:06 +0900

    Update build.gradle.kts

--- StremioX/build.gradle.kts
+++ StremioX/build.gradle.kts
@@ -1,26 +1,25 @@
-// v1.13
-import java.util.Properties
+// v1.10
+import java.util.Properties // org.jetbrains.kotlin... 대신 표준 java.util 사용
 
+// use an integer for version numbers
 version = 13
 
 android {
-    namespace = "com.phisher98"
     buildFeatures {
         buildConfig = true
         viewBinding = true
     }
     defaultConfig {
         val propFile = project.rootProject.file("local.properties")
-        val properties = Properties()
-        
-        // v1.13: 파일이 없을 경우 에러 대신 빈 값 처리 (CI 환경 대응)
         val tmdbApiKey = if (propFile.exists()) {
+            val properties = Properties()
             propFile.inputStream().use { properties.load(it) }
             properties.getProperty("TMDB_API") ?: ""
         } else {
             System.getenv("TMDB_API") ?: ""
         }
         
+        android.buildFeatures.buildConfig = true
         buildConfigField("String", "TMDB_API", "\"$tmdbApiKey\"")
     }
 }
@@ -34,7 +33,10 @@ cloudstream {
     description = "[!] Requires Setup \n- StremioX allows you to use stream addons \n- StremioC allows you to use catalog addons"
     authors = listOf("Hexated,phisher98")
     status = 1 
-    tvTypes = listOf("TvSeries", "Movie")
+    tvTypes = listOf(
+        "TvSeries",
+        "Movie",
+    )
     requiresResources = true
     iconUrl = "https://raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/master/StremioX/icon.png"
 }
