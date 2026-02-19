ff408644207a865593e4f53382cae8062ac4dddc
hsp1020 <125277818+hsp1020@users.noreply.github.com>
2026-02-20 06:00:57 +0900

    Update build.gradle.kts

--- StremioX/build.gradle.kts
+++ StremioX/build.gradle.kts
@@ -1,29 +1,26 @@
-// v1.4.
+// v2.0
 import java.util.Properties
 
 // use an integer for version numbers
 version = 13
 
 android {
+    namespace = "com.phisher98" // Gradle 8.0+ 필수 추가 (이 코드가 없으면 BuildConfig가 생성되지 않음)
+
     buildFeatures {
         buildConfig = true
         viewBinding = true
     }
     defaultConfig {
         val propFile = project.rootProject.file("local.properties")
-        println("[v1.4 Debug] Gradle Config - local.properties 파일 존재 여부: ${propFile.exists()}")
         
         val tmdbApiKey = if (propFile.exists()) {
-            println("[v1.4 Debug] local.properties 파일에서 TMDB_API 값을 읽어옵니다.")
             val properties = Properties()
             properties.load(propFile.inputStream())
             properties.getProperty("TMDB_API") ?: ""
         } else {
-            println("[v1.4 Debug] local.properties 파일이 없습니다. 환경변수 또는 기본값을 사용합니다.")
             System.getenv("TMDB_API") ?: ""
         }
-
-        println("[v1.4 Debug] 설정된 TMDB_API 길이: ${tmdbApiKey.length}")
         
         android.buildFeatures.buildConfig = true
         buildConfigField("String", "TMDB_API", "\"$tmdbApiKey\"")
@@ -40,7 +37,7 @@ cloudstream {
  // All of these properties are optional, you can safely remove them
 
      description = "[!] Requires Setup \n- StremioX allows you to use stream addons \n- StremioC allows you to use catalog addons"
-     authors = listOf("Hexated,phisher98")
+     authors = listOf("Hexated", "phisher98")
 
     /**
      * Status int as the following:
