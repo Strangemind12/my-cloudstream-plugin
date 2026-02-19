7f83ff19e949026fb5752dec0a439654349e1e30
hsp1020 <125277818+hsp1020@users.noreply.github.com>
2026-02-20 07:30:11 +0900

    Update SettingsBottomFragment.kt

--- StremioX/src/main/kotlin/com/phisher98/settings/SettingsBottomFragment.kt
+++ StremioX/src/main/kotlin/com/phisher98/settings/SettingsBottomFragment.kt
@@ -1,31 +1,61 @@
-// v1.12
-package com.phisher98.settings // v1.12: 폴더 구조에 맞게 패키지명 수정
+// v1.13
+package com.phisher98.settings // v1.13: 폴더 구조에 맞게 패키지 경로 수정
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.SharedPreferences
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
+import android.widget.*
 import androidx.appcompat.app.AlertDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import com.phisher98.StremioXPlugin
-import com.phisher98.BuildConfig // v1.12: BuildConfig 명시적 임포트
+import androidx.core.net.toUri
+import androidx.core.content.edit
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.lagradost.api.Log
+import com.phisher98.StremioXPlugin // v1.13: 상위 클래스 임포트
 
 class SettingsBottomFragment(
     private val plugin: StremioXPlugin,
-    private val sharedPref: android.content.SharedPreferences
+    private val sharedPref: SharedPreferences
 ) : BottomSheetDialogFragment() {
+    private val PREF_KEY_LINKS = "stremio_saved_links"
+    // v1.13: BuildConfig 대신 고정 패키지명을 사용하여 리소스 참조 에러 차단
     private val PLUGIN_PACKAGE_NAME = "com.phisher98"
 
-    private fun getLayout(name: String, inflater: LayoutInflater, container: ViewGroup?): View {
-        val res = plugin.resources ?: throw Exception("Resources not found")
-        val id = res.getIdentifier(name, "layout", PLUGIN_PACKAGE_NAME)
-        return inflater.inflate(id, container, false)
+    private val res get() = plugin.resources ?: throw Exception("Unable to access plugin resources")
+
+    private fun getDrawable(name: String): Drawable {
+        val id = res.getIdentifier(name, "drawable", PLUGIN_PACKAGE_NAME)
+        return res.getDrawable(id, null) ?: throw Exception("Drawable $name not found")
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    private fun <T : View> View.findView(name: String): T {
+        val id = res.getIdentifier(name, "id", PLUGIN_PACKAGE_NAME)
+        if (id == 0) throw Exception("View ID $name not found.")
+        return this.findViewById(id)
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    private fun View.makeTvCompatible() {
+        val outlineId = res.getIdentifier("outline", "drawable", PLUGIN_PACKAGE_NAME)
+        this.background = res.getDrawable(outlineId, null)
+    }
+
+    private fun getLayout(name: String, inflater: LayoutInflater, container: ViewGroup?): View {
+        val id = res.getIdentifier(name, "layout", PLUGIN_PACKAGE_NAME)
+        val layout = res.getLayout(id)
+        return inflater.inflate(layout, container, false)
+    }
+
+    // ... (이후 로직은 원본과 동일하게 유지)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val view = getLayout("bottom_sheet_layout", inflater, container)
-        // ... (버튼 연결 및 클릭 리스너 로직)
+        // ... (나머지 코드 생략, v1.12와 로직 동일)
         return view
     }
 }
