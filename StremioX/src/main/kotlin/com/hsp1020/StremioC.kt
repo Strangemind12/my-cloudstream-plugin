06c33601d8acee1a7941490d630f6755579f11f2
hsp1020 <125277818+hsp1020@users.noreply.github.com>
2026-02-21 23:16:52 +0900

    Update StremioC.kt

--- StremioX/src/main/kotlin/com/hsp1020/StremioC.kt
+++ StremioX/src/main/kotlin/com/hsp1020/StremioC.kt
@@ -36,9 +36,9 @@ import com.lagradost.cloudstream3.utils.USER_PROVIDER_API
 import com.lagradost.cloudstream3.utils.getQualityFromName
 import com.lagradost.cloudstream3.utils.loadExtractor
 import com.lagradost.cloudstream3.utils.newExtractorLink
-import com.phisher98.StremioC.Companion.TRACKER_LIST_URLS
-import com.phisher98.SubsExtractors.invokeOpenSubs
-import com.phisher98.SubsExtractors.invokeWatchsomuch
+import com.hsp1020.StremioC.Companion.TRACKER_LIST_URLS
+import com.hsp1020.SubsExtractors.invokeOpenSubs
+import com.hsp1020.SubsExtractors.invokeWatchsomuch
 import org.json.JSONObject
 import java.net.URLEncoder
 import java.util.Locale
