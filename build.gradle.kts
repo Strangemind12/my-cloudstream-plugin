0e27b627f4cc9b7e40119bb62d1728687060a10d
hsp1020 <125277818+hsp1020@users.noreply.github.com>
2026-03-07 12:11:30 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.3")
-        classpath("com.github.recloudstream:gradle:cce1b8d84d")
+        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.0")
     }
 }
