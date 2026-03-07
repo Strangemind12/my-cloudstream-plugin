3b89b752fab7ac5c483fdccdcc055a7cb465a7ff
hsp1020 <125277818+hsp1020@users.noreply.github.com>
2026-03-08 02:30:23 +0900

    Update build.gradle.kts

--- StreamedProvider/build.gradle.kts
+++ StreamedProvider/build.gradle.kts
@@ -1,46 +1,13 @@
-// 버전 정보: v1.0
-import com.lagradost.cloudstream3.plugins.CloudstreamPluginConfiguration
-
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
-    }
-    dependencies {
-        classpath("com.lagradost.cloudstream3.plugins:gradle-plugin:0.1.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-    }
-}
-
-apply(plugin = "com.android.library")
-apply(plugin = "kotlin-android")
-apply(plugin = "com.lagradost.cloudstream3.plugins")
+// 버전 정보: v1.1
+version = 1
 
 cloudstream {
-    setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/user/repo")
-}
-
-android {
-    namespace = "com.streamed"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 21
-        targetSdk = 34
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-}
-
-dependencies {
-    implementation("com.lagradost:cloudstream3:pre-release")
-    implementation("org.jsoup:jsoup:1.17.2")
+    description = "Streamed(Live Sports)"
+    authors = listOf("hsp1020")
+    language = "en"
+    status = 1 // 0: Down, 1: Ok, 2: Slow, 3: Beta
+    tvTypes = listOf(
+        "Live"
+    )
+    iconUrl = "https://streamed.pk/favicon.png"
 }
